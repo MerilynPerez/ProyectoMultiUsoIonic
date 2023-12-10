e85ec400afd1735b922d48cca759cb7f7afbe2b8
Marjhory Alexander De Los Santos Asencio <117186393+K0RASAN@users.noreply.github.com>
2023-12-10 19:18:55 -0400

    Update clima.page.ts

--- src/app/clima/clima.page.ts
+++ src/app/clima/clima.page.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { WeatherService } from '../servicios/weather.service';
 
 @Component({
   selector: 'app-clima',
   templateUrl: './clima.page.html',
   styleUrls: ['./clima.page.scss'],
 })
-export class ClimaPage implements OnInit {
+export class ClimaPage {
+  latitude: number = 0;
+  longitude: number = 0;
+  weatherData: any;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(public weatherService: WeatherService) {}
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
   }
 
+  getWeather() {
+    if (this.latitude && this.longitude) {
+      this.weatherService.getWeatherByCoords(this.latitude, this.longitude)
+        .then((response: { data: any; }) => {
+          this.weatherData = response.data;
+        })
+        .catch((error: any) => {
+          console.error(error);
+        });
+    }
+  }
 }
