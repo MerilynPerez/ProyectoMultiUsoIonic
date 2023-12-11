e49ff1ef5c663cc1035d94cbf64dc63a07e1655d
Marjhory Alexander De Los Santos Asencio <117186393+K0RASAN@users.noreply.github.com>
2023-12-10 23:52:16 -0400

    Update login.page.ts

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage{
+  usuario: string = "";
+  clave: string = "";
 
-  constructor() { }
+  constructor(private router: Router) {}
 
-  ngOnInit() {
+  validarAcceso() {
+    if (this.usuario === 'AgenteAdmin' && this.clave === '1234') {
+      this.router.navigate(['/aplicar-multa']); 
+    } else {
+      alert('Acceso denegado. Verifica tus credenciales.');
+    }
   }
-
 }
