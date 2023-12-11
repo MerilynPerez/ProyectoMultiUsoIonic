b3412b52d38212c30625c4e6fb22aedd2f78d4ca
Marjhory Alexander De Los Santos Asencio <117186393+K0RASAN@users.noreply.github.com>
2023-12-10 22:47:07 -0400

    Update horoscopo.page.ts

--- src/app/horoscopo/horoscopo.page.ts
+++ src/app/horoscopo/horoscopo.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {HoroscopoService } from '../servicios/horoscope.service';
 
 @Component({
   selector: 'app-horoscopo',
   templateUrl: './horoscopo.page.html',
   styleUrls: ['./horoscopo.page.scss'],
 })
-export class HoroscopoPage implements OnInit {
+export class HoroscopoPage {
+  signo: string = "";
+  horoscopo: string = "";
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(public horoscopoService: HoroscopoService) {}
+
+  obtenerHoroscopo() {
+    this.horoscopo = this.horoscopoService.getHoroscopo(this.signo);
   }
-
 }
