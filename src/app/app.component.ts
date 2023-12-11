60a5158bb4f407c4f42c7e6e241f0db90072e25e
Marjhory Alexander De Los Santos Asencio <117186393+K0RASAN@users.noreply.github.com>
2023-12-10 23:51:51 -0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
+  constructor(private router: Router) {
+    
+  }
   public appPages = [
     { title: 'Aplicar una multa', url: 'aplicar-multa', icon: 'newspaper' },
     { title: 'Ver clima', url: 'clima', icon: 'cloudy-night' },
@@ -13,8 +17,10 @@ export class AppComponent {
     { title: 'Multas registradas', url: 'multas-registradas', icon: 'document' },
     { title: 'Ver noticias', url: 'noticias', icon: 'clipboard' },
     { title: 'Tarifario de multas', url: 'multas-lista', icon: 'file-tray' },
-    { title: '', url: 'login', icon: 'person-circle' },
-    { title: 'Consulta vehiculo', url: 'consulta-vehiculo', icon: 'person-circle' },
+    { title: 'Consulta vehiculo', url: 'consulta-vehiculo', icon: 'car-sport' },
   ];
-  
+  cerrarSesion() {
+    console.log('Cerrando sesión...');
+    this.router.navigate(['/login']);
+  }
 }
